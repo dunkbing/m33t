8c58552a423ac27768c1538e4f28d41bef61c6c5
binh.buidang <dangbinh4869@gmail.com>
2023-01-26 15:14:52 +0700

    feat: copy meeting link to clipboard.

--- islands/Options.tsx
+++ islands/Options.tsx
@@ -1,4 +1,4 @@
-import { useState } from "preact/hooks";
+import { useCallback, useEffect, useState } from "preact/hooks";
 import { JSX } from "preact/jsx-runtime";
 import IconMicrophone from "ticons/tsx/microphone.tsx";
 import IconMicrophoneOff from "ticons/tsx/microphone-off.tsx";
@@ -6,6 +6,7 @@ import IconVideo from "ticons/tsx/video.tsx";
 import IconVideoOff from "ticons/tsx/video-off.tsx";
 import IconMessage from "ticons/tsx/message.tsx";
 import IconScreenShare from "ticons/tsx/screen-share.tsx";
+import IconInfoCircle from "ticons/tsx/info-circle.tsx";
 
 function OptionWrap(props: JSX.HTMLAttributes<HTMLDivElement>) {
   return (
@@ -45,28 +46,71 @@ function MediaButton(props: MediaButtonProps) {
   );
 }
 
+function Info() {
+  const [link, setLink] = useState("");
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    if (window.location) {
+      setLink(window.location.href);
+    }
+  }, [window.location]);
+
+  const copy = useCallback(() => {
+    if (navigator.clipboard) {
+      navigator.clipboard
+        .writeText(link)
+        .then(() => {
+          setCopied(true);
+          setTimeout(() => setCopied(false), 2000);
+        })
+        .catch(console.error);
+    }
+  }, [navigator.clipboard, link]);
+
+  return (
+    <div class="flex flex-col items-center bg-white rounded-md p-1.5 mb-1.5">
+      <p class="italic">Share this link to start talking.</p>
+      <div class="flex flex-row gap-2">
+        <p class="text-blue-600">{link}</p>
+        <p class="font-bold cursor-pointer" onClick={copy}>
+          {copied ? "Copied ✅" : "Copy"}
+        </p>
+      </div>
+    </div>
+  );
+}
+
 type OptionsProps = {
   onToggleAudio?: () => void;
   onToggleVideo?: () => void;
 };
 
 export default function Options(props: OptionsProps) {
+  const [showInfo, setShowInfo] = useState(true);
+
   return (
-    <div class="flex flex-row w-full md:w-1/2 lg:w-1/5 gap-2 items-center">
-      <MediaButton type="audio" onToggle={props.onToggleAudio} />
-      <MediaButton type="video" onToggle={props.onToggleVideo} />
-      <OptionWrap>
-        <IconMessage size={36} />
-      </OptionWrap>
-      <OptionWrap>
-        <IconScreenShare size={36} />
-      </OptionWrap>
-      <button
-        class="px-4 py-3.5 bg-red-400 font-bold text-white border border-b-4 border-r-4 border-red-600 rounded-lg shadow-lg hover:bg-red-500 hover:shadow-sm hover:border-b-2 hover:border-r-2"
-        onClick={() => close?.()}
-      >
-        Leave
-      </button>
+    <div class="flex flex-col items-center">
+      {showInfo && <Info />}
+      <div class="flex flex-row gap-2 items-center">
+        <OptionWrap onClick={() => setShowInfo(!showInfo)}>
+          <IconInfoCircle size={36} />
+        </OptionWrap>
+        <MediaButton type="audio" onToggle={props.onToggleAudio} />
+        <MediaButton type="video" onToggle={props.onToggleVideo} />
+        <OptionWrap>
+          <IconMessage size={36} />
+        </OptionWrap>
+        <OptionWrap>
+          <IconScreenShare size={36} />
+        </OptionWrap>
+        <button
+          class="px-4 py-3.5 bg-red-400 font-bold text-white border border-b-4 border-r-4 border-red-600 rounded-lg shadow-lg hover:bg-red-500 hover:shadow-sm hover:border-b-2 hover:border-r-2"
+          onClick={() => close?.()}
+        >
+          Leave
+        </button>
+      </div>
     </div>
   );
 }
